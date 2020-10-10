1ab74d04eaaee8949942aaab52412392e81b7849
Elliot Stjernqvist <elliot.stjernqvist@gmail.com>
2020-10-10 12:25:58 +0200

    Large updates to code for deciding to turn left or right,  untested as of now

--- src/sensor.cpp
+++ src/sensor.cpp
@@ -1,20 +1,32 @@
-#include <NewPing.h>
+#include <NewPing.h> //Function library for ultrasonic sensor, must be installed
 #include <Arduino.h>
-#include <Servo.h>
+#include <Servo.h> //Servo motor library, standard but may need to be installed
+
+//Ultrasonic sensor pins
 const unsigned int TRIG_PIN = 13;
 const unsigned int ECHO_PIN = 12;
+
+//Communication baud rate
 const unsigned int BAUD_RATE = 9600;
-//L293D
-//Motor A
-const int rightForward  = 5;  // Pin 14 of L293
-const int rightBackward = 4;  // Pin 10 of L293
-//Motor B
-const int leftForward  = 3; // Pin  7 of L293
-const int leftBackward  = 2;  // Pin  2 of L293
+
+//L293D control pins
+const int rightForward  = 5;  
+const int rightBackward = 4;  
+const int leftForward  = 3; 
+const int leftBackward  = 2;  
+
+//Maximum distance, over this the sensor does not need to measure exact and will return 0
 int maximum_distance = 200;
+
+//Bolean to check if car is moving forward
+boolean goesForward = false;
+
+//Distance variable for storing sensor distance measurment
 int distance = 100;
-NewPing sonar(TRIG_PIN, ECHO_PIN, maximum_distance);
-Servo myservo;
+
+NewPing sonar(TRIG_PIN, ECHO_PIN, maximum_distance); //sensor function using NewPing library
+
+Servo myservo; //servo name
 
 int readPing(){
   delay(70);
@@ -25,6 +37,92 @@ int readPing(){
   return cm;
 }
 
+//Function to look right and return distance sensor value
+int lookRight(){ 
+  myservo.write(10);
+  delay(500);
+  int distance = readPing();
+  delay(100);
+  myservo.write(90);
+  return distance;
+}
+
+//Function to look left and return distance sensor value
+int lookLeft(){
+  myservo.write(170);
+  delay(500);
+  int distance = readPing();
+  delay(100);
+  myservo.write(90);
+  return distance;
+  delay(100);
+}
+
+//Function to stop all motors
+void moveStop(){
+  digitalWrite(rightForward, LOW);
+  digitalWrite(leftForward, LOW);
+  digitalWrite(rightBackward, LOW);
+  digitalWrite(leftBackward, LOW);
+}
+
+//Function to move forward
+void moveForward(){
+  if(!goesForward){
+    goesForward = true;
+
+    digitalWrite(leftForward, HIGH);
+    digitalWrite(rightForward, HIGH);
+
+    digitalWrite(leftBackward, LOW);
+    digitalWrite(rightBackward, LOW);
+  }
+}
+
+//Function to move backwards
+void moveBackward(){
+  goesForward = false;
+
+  digitalWrite(leftBackward, HIGH);
+  digitalWrite(rightBackward, HIGH);
+
+  digitalWrite(leftForward, LOW);
+  digitalWrite(rightForward, LOW);
+}
+
+//Function to turn right
+void turnRight(){
+  digitalWrite(leftForward, HIGH);
+  digitalWrite(rightBackward, HIGH);
+
+  digitalWrite(leftBackward, LOW);
+  digitalWrite(rightForward, LOW);
+
+  delay(500);
+
+  digitalWrite(leftForward, HIGH);
+  digitalWrite(rightForward, HIGH);
+
+  digitalWrite(leftBackward, LOW);
+  digitalWrite(rightBackward, LOW);
+}
+
+//Function to turn left
+void turnLeft(){
+  digitalWrite(leftBackward, HIGH);
+  digitalWrite(rightForward, HIGH);
+  
+  digitalWrite(leftForward, LOW);
+  digitalWrite(rightBackward, LOW);
+
+  delay(500);
+
+  digitalWrite(leftForward, HIGH);
+  digitalWrite(rightForward, HIGH);
+
+  digitalWrite(leftBackward, LOW);
+  digitalWrite(rightBackward, LOW);
+}
 
 //This will run only one time.
 void setup(){
@@ -40,15 +138,16 @@ void setup(){
   pinMode(TRIG_PIN, OUTPUT);
   pinMode(ECHO_PIN, INPUT);
   
-  distance = readPing();
-  delay(100);
-  distance = readPing();
-  delay(100);
-  distance = readPing();
-  delay(100);
-  distance = readPing();
-  delay(100);
   myservo.write(90);
+  delay(2000);
+  distance = readPing();
+  delay(100);
+  distance = readPing();
+  delay(100);
+  distance = readPing();
+  delay(100);
+  distance = readPing();
+  delay(100);
   delay(4000);
    
   
